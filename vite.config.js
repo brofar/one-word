86c88a9f75f1f3785232f579daf3615655543d23
brofar <behfar@otafest.com>
2025-03-22 20:41:10 -0600

    change deploy script to vite's recommendation

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/one-word/', // This is the name of the repository
 })
